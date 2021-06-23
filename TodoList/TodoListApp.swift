985d80afefa4ea7f240d8e43fb9709d2aaea2e15
Ome Asraf <omeasraf0@gmail.com>
2021-06-23 10:17:41 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Ome Asraf on 6/23/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
