597bac3af4fec6cb4fcc40fc34d7dddac87e9590
Ome Asraf <omeasraf0@gmail.com>
2021-06-23 16:10:39 -0400

    Update ListViewModel.swift
    Saving data to the device storage

--- TodoList/ViewModels/ListViewModel.swift
+++ TodoList/ViewModels/ListViewModel.swift
@@ -8,16 +8,28 @@
 import Foundation
 
 class ListViewModel: ObservableObject {
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItem()
+        }
+    }
     @Published var completedCount:Int = 0;
+    let itemsKey:String = "item_list"
+    let completedKey:String = "completed_item_count"
     
     init() {
         getItems()
     }
     
     func getItems(){
-        let newItems:[ItemModel] = []
-        items.append(contentsOf: newItems)
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data),
+            let cCount:Int = UserDefaults.standard.value(forKey: completedKey) as? Int
+        else { return }
+        
+        self.items = savedItems
+        self.completedCount = cCount
     }
     
     func deleteItem(indexSet: IndexSet){
@@ -40,38 +52,65 @@ class ListViewModel: ObservableObject {
             if items[index].isCompleted {
                 completedCount += 1
                 if completedCount == 1 {
-                    items.swapAt(index, 0)
+                    items.move(from: index, to: 0)
                 }
                 else{
                     if let firstIdx = items.firstIndex(where: {
-                        $0.isCompleted == true
+                        $0.id != item.id && $0.isCompleted == true
                     }){
-                        items.swapAt(index, firstIdx)
+                        items.move(from: index, to: firstIdx)
+                    }
+                    else{
+  
+                        items.move(from: index, to: 0)
                     }
                 }
             }
             else{
-                completedCount -= 1
+                if (completedCount > 0){
+                    completedCount -= 1
+                }
                 if completedCount == 0 {
-                    items.swapAt(index, 0)
+                    items.move(from: index, to: 0)
                 }
                 else{
                     if let firstIdx = items.firstIndex(where: {
                         $0.id != item.id && $0.isCompleted == false
                     }){
-                        items.swapAt(index, firstIdx != 0 ? firstIdx - 1 : firstIdx)
+                        items.move(from: index, to: firstIdx != 0 ? firstIdx - 1 : firstIdx)
                     }
                     else{
-                        items.swapAt(index, items.endIndex - 1)
+                        items.move(from: index, to: items.endIndex - 1)
+                     
                     }
                 }
             }
         }
+        saveItem()
+        print("Completed: " + String(completedCount))
     }
     
     func saveItem() {
         if let encodedData = try? JSONEncoder().encode(items){
-            UserDefaults.standard.set(encodedData, forKey: "item_list")
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
+            UserDefaults.standard.set(completedCount, forKey: completedKey)
         }
+    }
+}
+
+
+extension Array where Element: Equatable
+{
+    mutating func move(_ element: Element, to newIndex: Index) {
+        if let oldIndex: Int = self.firstIndex(of: element) { self.move(from: oldIndex, to: newIndex) }
+    }
+}
+
+extension Array
+{
+    mutating func move(from oldIndex: Index, to newIndex: Index) {
+        if oldIndex == newIndex { return }
+        if abs(newIndex - oldIndex) == 1 { return self.swapAt(oldIndex, newIndex) }
+        self.insert(self.remove(at: oldIndex), at: newIndex)
     }
 }
